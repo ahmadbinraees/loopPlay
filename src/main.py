d866699ceaa00e7c98aed9c96894408d4062ab96
root <root@localhost>
2022-04-28 04:54:36 +0600

    auto publish 1651100076609

---
+++ src/main.py
@@ -0,0 +1,28 @@
+# there are two type loop in py
+# 1- for loop, 2- while loop
+
+names = ["Ron","Jon","hon"]
+for name in names:
+  print(name)
+
+
+
+for fname in names:
+  fname = "This is a name " + fname
+  print(fname)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
